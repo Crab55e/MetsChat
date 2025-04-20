5e7e116133925cbb8d670668a5b175b6a92e7bf0
Crab55e <crabkani807@gmail.com>
2025-04-20 15:19:37 +0900

    discordのメッセージがマイクラに届くように

--- src/main/kotlin/cc/crab55e/metsChat/discord/MessageReceived.kt
+++ src/main/kotlin/cc/crab55e/metsChat/discord/MessageReceived.kt
@@ -1,29 +1,52 @@
 package cc.crab55e.metsChat.discord
 
 import cc.crab55e.metsChat.MetsChat
+import cc.crab55e.metsChat.util.PlaceholderFormatter
 import com.moandjiezana.toml.Toml
 import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.channel.ChannelType
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.kyori.adventure.text.minimessage.MiniMessage
 
 
-class MessageReceived(val plugin: MetsChat) : ListenerAdapter() {
+class MessageReceived(private val plugin: MetsChat) : ListenerAdapter() {
     private val logger = plugin.getLogger()
     private val config = plugin.getConfigManager().getConfig()
-    private val configDiscordTable: Toml = config.getTable("discord")
-    private val centralForumChannelID = configDiscordTable.getString("central-forum-channel-id")
+    private val channelIdsTable = config.getTable("discord.channel-ids")
+    private val toMinecraftTable = config.getTable("discord.message-share.to-minecraft")
+    private val mm = MiniMessage.miniMessage()
     override fun onMessageReceived(event: MessageReceivedEvent) {
         val message: Message = event.message
         val content: String = message.contentRaw
 
-        logger.info("Fired MessageReceivedEvent ${content}@${message.channelId} type ${message.channelType}")
+        val centralChatChannelId = channelIdsTable.getString("central-chat")
+        val minecraftMessageFormat = toMinecraftTable.getString("format")
 
-        if (message.channelId != centralForumChannelID || message.channelType != ChannelType.FORUM) return
+        if (message.channelId != centralChatChannelId) return
         if (event.author.isBot) return
+        if (!toMinecraftTable.getBoolean("enabled")) return
 
-        logger.info("starting main processing")
+        val markdownParsedMessage = "content"
+        val roleColorHex = "ff2222"
+        val allRoleNames = "admin, op, member"
+
+        val minecraftMessage = mm.deserialize( PlaceholderFormatter.format(
+            minecraftMessageFormat,
+            mapOf(
+                "authorName" to event.author.name,
+                "markdownParsedMessage" to markdownParsedMessage,
+                "roleColorHex" to roleColorHex,
+                "allRoleNames" to allRoleNames,
+                "discordMessageUrl" to event.jumpUrl
+            )
+        ))
+
+
+        plugin.getServer().allPlayers.forEach playerLoop@{
+            val receiver = it
+            receiver.sendMessage(minecraftMessage)
+        }
 
     }
 }
